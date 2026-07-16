10ae5ee25e91d260b8485f33893d803cb93bace2
Gabriel Oliveira Rodrigues <111239856+GabrielRodrigues-42@users.noreply.github.com>
2026-07-16 11:21:27 -0300

    Delete incorrect file

--- quicktest.py
+++
@@ -1,6 +0,0 @@
-for lib in ["errant", "nltk", "statsmodels", "openpyxl"]:
-    try:
-        mod = __import__(lib)
-        print(f"{lib}: {getattr(mod, '__version__', 'sem __version__')}")
-    except ModuleNotFoundError:
-        print(f"{lib}: NÃO INSTALADO")
